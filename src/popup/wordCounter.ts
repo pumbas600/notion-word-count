b42ba947abe9bc48e26b983e58ffae71a9f3a2a7
pumbas600 <joshjeffers600@gmail.com>
2023-06-09 12:09:09 +1200

    feat: :sparkles: Get notion page root

--- src/popup/wordCounter.ts
+++ src/popup/wordCounter.ts
@@ -1,4 +1,20 @@
+import { Maybe } from "../types";
 import { Block } from "./notion/blocks";
+
+let pageRoot: Maybe<Element> = undefined;
+
+function getPageRoot() {
+    if (pageRoot === undefined) {
+        const elements = document.getElementsByClassName('notion-page-content');
+        if (elements.length !== 0) {
+            throw new Error(`Expected there to only be one 'notion-page-content' but found ${elements.length}`);
+        }
+
+        pageRoot = elements[0];
+    }
+    
+    return pageRoot;
+}
 
 function countTextNodeWords(textNode: ChildNode): number {
     const value = textNode.nodeValue;
