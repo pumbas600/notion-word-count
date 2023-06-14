dbf7766b9c9bce084e0faebf72377834df919644
pumbas600 <joshjeffers600@gmail.com>
2023-06-14 23:55:16 +1200

    i18n: :globe_with_meridians: Determine what language to use for the extension

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,12 +1,29 @@
 import { Translations } from './translations';
 
 /**
+ * Finds the best language to display the text to the user in. This is the first language in the user's
+ * preferred languages that is supported by this extension. If no languages are supported, English is
+ * used as a fallback.
+ *
+ * @returns The language code to display the text in, e.g. "en"
+ */
+export function getUserDisplayLanguage(): string {
+  const supportedLanguages = getUserPrimaryLanguages().filter(isLanguageSupported);
+
+  if (supportedLanguages.length === 0) {
+    return 'en';
+  }
+
+  return supportedLanguages[0];
+}
+
+/**
  * Retrieves a list of the user's primary languages, in order of preference, with 0 being the highest
  * preference. This does not contain the region suffix or duplicates.
  *
  * @returns The ordered list of the user's primary languages, e.g. ["en", "fr"]
  */
-export function getUserPrimaryLanguages(): string[] {
+function getUserPrimaryLanguages(): string[] {
   return (
     navigator.languages
       .map(getPrimaryLanguageCode)
