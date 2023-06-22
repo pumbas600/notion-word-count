41aa37a716b5d680494b92a91bcc6eee34a28ce3
pumbas600 <joshjeffers600@gmail.com>
2023-06-22 19:03:34 +1200

    docs: :memo: Update release message

--- scripts/build.js
+++ scripts/build.js
@@ -97,8 +97,9 @@ function updateManifestVersions() {
 
 function pushRelease() {
   const commitMessage = `release: :label: v${packageJson.version}`;
+  const releaseMessage = `Release v${packageJson.version}. You can download the latest version by referring to the links in the README.md.`;
   exec(
-    `git commit -m "${commitMessage}" && git tag -a v${packageJson.version} -m "${commitMessage}" && git push --follow-tags`,
+    `git commit -m "${commitMessage}" && git tag -a v${packageJson.version} -m "${releaseMessage}" && git push --follow-tags`,
   );
 }
 
